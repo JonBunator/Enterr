03dc6eea7a73996361847a78d3a6d80eeebb7783
JonBunator <jonasmitb@gmail.com>
2025-02-16 14:25:30 +0100

    Fix main layout styling on chrome

--- src/frontend/src/components/pages/MainPage.tsx
+++ src/frontend/src/components/pages/MainPage.tsx
@@ -5,6 +5,7 @@ import Activity from '../activity/Activity.tsx'
 import Content from '../layout/Content.tsx'
 import AccountSettings from '../settings/AccountSettings.tsx'
 import ProtectedPage from './ProtectedPage.tsx'
+import './MainPage.scss'
 
 export default function MainPage() {
   const [searchTerm, setSearchTerm] = useState<string>('')
@@ -13,9 +14,13 @@ export default function MainPage() {
     <ProtectedPage>
       <AccountSettings />
       <Content>
-        <Search value={searchTerm} onChange={setSearchTerm} />
-        <AddWebsite />
-        <Activity searchTerm={searchTerm} />
+        <div className="main-page">
+          <div className="main-page-header">
+            <Search value={searchTerm} onChange={setSearchTerm} />
+            <AddWebsite />
+          </div>
+          <Activity searchTerm={searchTerm} />
+        </div>
       </Content>
     </ProtectedPage>
   )
